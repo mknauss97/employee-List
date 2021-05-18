c97d0a4248777fe349129009b47900b38bfd33a4
Mknauss <mknauss97@gmail.com>
2021-05-17 20:33:01 -0400

    added info to the tables

--- employeeListSeeds.sql
+++ employeeListSeeds.sql
@@ -29,6 +29,29 @@ CREATE TABLE department (
     name VARCHAR(30) 
 );
 
+INSERT INTO employee(first_name, last_name, role_id)
+VALUES ("Eden", "Hebert", 4)
+       ("Damien", "Harrington", 1)
+       ("Norah", "Duke", 2)
+       ("Jon", "Cantrell", 2)
+       ("Bradley", "Klyczek", 1)
+       ("Joel", "Conrad", 4)
+       ("Allison", "Vance", 5)
+       ("Neveah", "Mitchell", 3)
+
+INSERT INTO department(name)
+VALUES ("Accounting")
+       ("Maintenance")
+       ("Store")
+       ("Upper Management")
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Retail Accounting", 75000, 1,)
+       ("General Manager", 90000, 4,)
+       ("Retail Associate", 50000, 3,)
+       ("IT", 80000, 2,)
+       ("Store Manager", 60000, 3,)
+
 SELECT * FROM employee;
 SELECT * FROM role;
 SELECT * FROM department;
