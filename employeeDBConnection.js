2048e5bfd84dc277f8257f8a8bbf4a08fb96aaa9
Mknauss <mknauss97@gmail.com>
2021-05-19 19:22:29 -0400

    why wont it show me

--- employeeDBConnection.js
+++ employeeDBConnection.js
@@ -1,18 +1,94 @@
 const mysql = require('mysql');
+const inquirer  = require('inquirer');
+const util = require("util");
+
+
+
 
 const connection = mysql.createConnection({
-  host: 'localhost',
- 
-  port: 3306,
-  
-  user: 'root',
- 
-  password: 'root',
-  database: 'employee_listDB',
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'employee_listDB',
 });
 
 connection.connect((err) => {
-  if (err) throw err;
-  console.log(`connected as id ${connection.threadId}`);
-  connection.end();
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    connection.end();
 });
+
+const startApp = () => {
+
+    inquirer.prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+            'View all employees',
+            'View all employees by role',
+            'View all employees by department',
+            'Update employee',
+            'Add a new employee',
+            'Add a new role',
+            'Add a new department',
+            'Exit'
+        ],
+    })
+        .then((answer) => {
+            switch (answer.action) {
+                case 'View all employees':
+                    employeeSearch();
+                    break;
+
+                case 'View all employees by role':
+                    employeeRoleSearch();
+                    break;
+
+                case 'View all employees by department':
+                    employeeDepartmentSearch();
+                    break;
+
+                case 'Update employee':
+                    employeeUpdate(); 
+                    break;
+
+                case 'Add a new employee':
+                    addEmployee();
+                    break;
+
+                case 'Add a new role':
+                    addRole();
+                    break;
+
+                case 'Add a new department':
+                    addDepartment();
+                    break;
+
+                default:
+                    console.log('invalid action: ${answer.action}');
+                    break;
+
+            }
+        })
+};
+
+const employeeSearch = () => {
+    connection.query() = 'SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name AS department ';
+    query +=
+        'FROM employee LEFT JOIN role ON (employee.role_id = role.id) '
+    query +=
+        'LEFT JOIN department ON (department.id = role.department_id)'
+    const result = connection.query(res)
+    console.table(result)
+    startApp()
+}
+
+const employeeRoleSearch = () => {
+    let query = 
+    'SELECT * FROM role';
+    const roleData = connection.query(query);
+    console.table(roleData);
+    promptUser();
+}
--- employeeListSeeds.sql
+++ employeeListSeeds.sql
@@ -4,53 +4,79 @@ CREATE DATABASE employee_listDB;
 
 USE employee_listDB;
 
--- Creat employee table
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30), 
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+
 CREATE TABLE employee (
     id INT AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
---create role table
-CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT
-    PRIMARY KEY (id)
-);
 
---create department table
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) 
-);
 
-INSERT INTO employee(first_name, last_name, role_id)
-VALUES ("Eden", "Hebert", 4)
-       ("Damien", "Harrington", 1)
-       ("Norah", "Duke", 2)
-       ("Jon", "Cantrell", 2)
-       ("Bradley", "Klyczek", 1)
-       ("Joel", "Conrad", 4)
-       ("Allison", "Vance", 5)
-       ("Neveah", "Mitchell", 3)
+
 
 INSERT INTO department(name)
-VALUES ("Accounting")
-       ("Maintenance")
-       ("Store")
-       ("Upper Management")
+VALUE ("Accounting");
+INSERT INTO department(name)
+VALUE ("Maintenance");
+INSERT INTO department(name)
+VALUE ("Store");
+INSERT INTO department(name)
+VALUE ("Upper Management");
+
+
+
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Retail Accounting", 75000, 1,)
-       ("General Manager", 90000, 4,)
-       ("Retail Associate", 50000, 3,)
-       ("IT", 80000, 2,)
-       ("Store Manager", 60000, 3,)
+VALUE ("Retail Accounting", 75000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("General Manager", 90000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Retail Associate", 50000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("IT", 80000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Store Manager", 60000, 3);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Eden", "Hebert", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Damien", "Harrington", 1, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Norah", "Duke", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Jon", "Cantrell", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Bradley", "Klyczek", 1, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Joel", "Conrad", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Allison", "Vance", 5, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Neveah", "Mitchell", 3, 4);
 
 SELECT * FROM employee;
 SELECT * FROM role;
